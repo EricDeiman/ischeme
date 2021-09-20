d0fe0958f711e228bbe0d16d047b96940cc6b087
Eric <eric@system76-pc.localdomain>
2021-09-20 11:22:32 -0500

    add copyright and license

--- src/main.cc
+++ src/main.cc
@@ -1,3 +1,20 @@
+/*
+  The iScheme programming language
+  Copyright 2020 Eric J. Deiman
+
+  This file is part of the iScheme programming language.
+  The iScheme programming language is free software: you can redistribute it
+  and/ormodify it under the terms of the GNU General Public License as published by the
+  Free Software Foundation, either version 3 of the License, or (at your option) any
+  later version.
+  
+  The iScheme programming language is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+  You should have received a copy of the GNU General Public License along with the
+  iScheme programming language. If not, see <https://www.gnu.org/licenses/>
+*/
+
 
 #include <iostream>
 #include <sstream>
